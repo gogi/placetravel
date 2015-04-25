0f9dff0ad37ef4aee28078fa18e9173cd43c977e
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-04-25 20:29:28 +0200

    Add users to trip and show user as name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :trips
+  root 'trips#index'
 
   devise_for :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424212137) do
+ActiveRecord::Schema.define(version: 20150424214135) do
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "location"
+    t.date     "date"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "trips", ["user_id"], name: "index_trips_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
