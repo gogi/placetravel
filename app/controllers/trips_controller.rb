58f7c9cc7e1e5f70e6ddaefb8b960800a673bad7
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-04-27 15:26:50 +0200

    Add decent_exposure to trips_controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,9 @@
 class TripsController < ApplicationController
 
-  expose(:trip)
+  # expose(:trip)
   expose(:trips)
+  expose(:trip, attributes: :trip_params)
+  expose(:user)
 
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
@@ -15,7 +17,7 @@ class TripsController < ApplicationController
   # GET /trips
   # GET /trips.json
   def index
-    trips = Trip.all
+    # trips = Trip.all
   end
 
   # GET /trips/1
@@ -35,7 +37,7 @@ class TripsController < ApplicationController
   # POST /trips
   # POST /trips.json
   def create
-    trip = Trip.new(trip_params)
+    # trip = Trip.new(trip_params)
     trip.user = current_user
 
     respond_to do |format|
@@ -53,12 +55,12 @@ class TripsController < ApplicationController
   # PATCH/PUT /trips/1.json
   def update
     respond_to do |format|
-      if @trip.update(trip_params)
-        format.html { redirect_to @trip, notice: 'Trip was successfully updated.' }
-        format.json { render :show, status: :ok, location: @trip }
+      if trip.update(trip_params)
+        format.html { redirect_to trip, notice: 'Trip was successfully updated.' }
+        format.json { render :show, status: :ok, location: trip }
       else
         format.html { render :edit }
-        format.json { render json: @trip.errors, status: :unprocessable_entity }
+        format.json { render json: trip.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -66,7 +68,7 @@ class TripsController < ApplicationController
   # DELETE /trips/1
   # DELETE /trips/1.json
   def destroy
-    @trip.destroy
+    trip.destroy
     respond_to do |format|
       format.html { redirect_to trips_url, notice: 'Trip was successfully destroyed.' }
       format.json { head :no_content }
@@ -76,7 +78,7 @@ class TripsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_trip
-      @trip = Trip.find(params[:id])
+      trip = Trip.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
