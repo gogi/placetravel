f46265a263d02f533ff57ee3c3ea5b073369c68a
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-04-29 19:07:46 +0200

    Fix name saving for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
   
 end
