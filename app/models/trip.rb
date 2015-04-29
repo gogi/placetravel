dc9e7c27639a89859f9fca6a11d0e389e0f0daa6
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-04-29 20:11:09 +0200

    Add alias to user and users

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,10 @@ class Trip < ActiveRecord::Base
   has_many :places
   has_many :memberships
   has_many :users, through: :memberships
+
+  alias_method :owner, :user
+  alias_method :owner=, :user=
+
+  alias_method :members, :users
+  alias_method :members=, :users=
 end
