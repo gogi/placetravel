9b43f3d936bae68973e186ab922e87650071c229
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-01 14:12:44 +0200

    Rubocop- fix too long line

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -85,7 +85,14 @@ class TripsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def trip_params
-      params.require(:trip).permit(:name, :description, :location, :date, :user_id)
+      params.require(:trip).permit(
+        :name,
+        :description,
+        :location,
+        :date,
+        :user_id,
+        :longitude,
+        :latitude)
     end
 
     def invite_params
