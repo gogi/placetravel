17051ce6c75b2c70f6ab3aad60b48ea5a46bc18a
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-18 19:07:22 +0200

    Add rspec for membership in trip model

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :trip do
     name "MyString"
-description "MyText"
-location "MyString"
-date "2015-04-24"
-user nil
+    description "MyText"
+    address "London"
+    date "2015-04-24"
+    user nil
   end
 
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user1) { create(:user, email: 'user@wp.pl') }
+  let(:user2) { create(:user, email: 'user2@wp.pl') }
+  let(:user3) { create(:user, email: 'user3@wp.pl') }
+  let(:trip) { create(:trip, users: [user1, user2]) }
+
+  it 'finds user who is a member of a trip' do
+    membership = trip.membership(user1)
+    expect(membership.user).to eq user1
+  end
+
+  it 'is nil for user not assigned to trip' do
+    membership = trip.membership(user3)
+    expect(membership).to eq nil
+  end
 end
