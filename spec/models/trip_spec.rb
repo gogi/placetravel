ed6a331ecb391b528173a43857fa956828ed5db6
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-18 20:44:16 +0200

    Add specs for method members in trip model

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -7,13 +7,37 @@ RSpec.describe Trip, type: :model do
   let(:user3) { create(:user, email: 'user3@wp.pl') }
   let(:trip) { create(:trip, users: [user1, user2]) }
 
-  it 'finds user who is a member of a trip' do
-    membership = trip.membership(user1)
-    expect(membership.user).to eq user1
+  describe '#membership' do
+    it 'finds user who is a member of a trip' do
+      membership = trip.membership(user1)
+      expect(membership.user).to eq user1
+    end
+
+    it 'is nil for user not assigned to trip' do
+      membership = trip.membership(user3)
+      expect(membership).to eq nil
+    end
   end
 
-  it 'is nil for user not assigned to trip' do
-    membership = trip.membership(user3)
-    expect(membership).to eq nil
+  describe '#members' do
+    it 'returns array of users that are active members of trip' do
+      membership = trip.membership(user1)
+      membership.active = true
+      membership.save
+      members = trip.members
+      expect(members).to include user1
+    end
+    it 'returns empty array if user is inactive member of trip' do
+      membership = trip.membership(user2)
+      membership.active = false
+      membership.save
+      members = trip.members
+      expect(members).to be_empty
+    end
+    it 'returns empty array if user is not member of a trip' do
+      membership = trip.membership(user3)
+      members = trip.members
+      expect(members).to be_empty
+    end
   end
 end
