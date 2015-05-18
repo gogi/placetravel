b609cdaa41f939c47054b33716231d3e6999ea30
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-19 00:58:21 +0200

    Add specs 'expose place' for GET #show in place controller

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -12,10 +12,11 @@ describe PlacesController do
       expect(response).to render_template :show
     end
 
-    # it 'assigns new place to @place' do
-    #   place = create(:place)
-    #   get :show, id: place
-    #   expect(assigns(:place)).to eq place
-    # end
+    it 'exposes place' do
+      trip = create(:trip)
+      place = create(:place, trip: trip)
+      get :show, trip_id: trip.id, id: place.id
+      expect(controller.place).to eq place
+    end
   end
 end
