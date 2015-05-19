46502dee9549a279ea08642c2fd5a6503a0cafb4
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-19 19:34:20 +0200

    Add spec GET #index for places_controller

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -3,20 +3,36 @@ require 'rails_helper'
 describe PlacesController do
   
   describe 'GET #show' do
-    login_user
+    context 'logged user' do
+      login_user
 
-    it 'it renders the :show template' do
-      trip = create(:trip)
-      place = create(:place, trip: trip)
-      get :show, trip_id: trip.id, id: place.id
-      expect(response).to render_template :show
+      describe 'GET #show' do
+        it 'it renders the :show template' do
+          trip = create(:trip)
+          place = create(:place, trip: trip)
+          get :show, trip_id: trip.id, id: place.id
+          expect(response).to render_template :show
+        end
+
+        it 'exposes place' do
+          trip = create(:trip)
+          place = create(:place, trip: trip)
+          get :show, trip_id: trip.id, id: place.id
+          expect(controller.place).to eq place
+        end
+      end
+
+      describe 'GET #index' do
+        it 'it renders the :index template' do
+          trip = create(:trip)
+          get :index, trip_id: trip.id
+          expect(response).to render_template :index
+        end
+      end
     end
 
-    it 'exposes place' do
-      trip = create(:trip)
-      place = create(:place, trip: trip)
-      get :show, trip_id: trip.id, id: place.id
-      expect(controller.place).to eq place
+    context 'not logged user' do
+
     end
   end
 end
