ab8f0279020d4085eb04b99dd146c6eca466d469
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-19 14:37:31 +0200

    Add validation specs for Place model

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Place, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:trip) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:address) }
 end
