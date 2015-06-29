1f9ec55f684d28b9778462ea99cf87af462ca6a0
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-06-29 16:17:57 +0200

    Add decorator places_display to trip index view

--- app/decorators/trip_decorator.rb
+++ app/decorators/trip_decorator.rb
@@ -5,4 +5,12 @@ class TripDecorator < Draper::Decorator
     object.date.strftime("%A %e, %B %Y")
   end
 
+  def places_display
+    if trip.places.count != 1
+      'Places'
+    else
+      'Places'.singularize
+    end
+  end
+
 end
