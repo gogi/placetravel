5e2ec1928331994690b30140f85d8973587466a1
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-11-17 22:12:28 +0100

    Specs for places controller

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -1,67 +1,89 @@
 require 'rails_helper'
 
 describe PlacesController do
-  
-  describe 'GET #show' do
-    context 'logged user' do
-      login_user
 
-      describe 'GET #show' do
-        it 'it renders the :show template' do
-          trip = create(:trip)
-          place = create(:place, trip: trip)
-          get :show, trip_id: trip.id, id: place.id
-          expect(response).to render_template :show
-        end
+  context 'logged user' do
+    login_user
+    let(:trip) { create(:trip) }
 
-        it 'exposes place' do
-          trip = create(:trip)
-          place = create(:place, trip: trip)
-          get :show, trip_id: trip.id, id: place.id
-          expect(controller.place).to eq place
-        end
+    describe 'GET #show' do
+      it 'it renders the :show template' do
+        place = create(:place, trip: trip)
+        get :show, trip_id: trip.id, id: place.id
+        expect(response).to render_template :show
       end
 
-      describe 'GET #index' do
-        it 'it renders the :index template' do
-          trip = create(:trip)
-          get :index, trip_id: trip.id
-          expect(response).to render_template :index
-        end
-      end
-
-      describe 'GET #new' do
-        it 'renders the :new template' do
-          trip = create(:trip)
-          get :new, trip_id: trip.id
-          expect(response).to render_template :new
-        end
-        it 'exposes place' do
-          trip = create(:trip)
-          place = create(:place, trip: trip)
-          get :new, trip_id: trip.id, id: place.id
-          expect(controller.place).to eq place
-        end
-      end
-
-      describe 'GET #edit' do
-        it 'renders the :edit template' do
-          trip = create(:trip)
-          place = create(:place, trip: trip)
-          get :edit, trip_id: trip.id, id: place.id
-          expect(response).to render_template :edit
-        end
-        it 'exposes place' do
-          trip = create(:trip)
-          place = create(:place, trip: trip)
-          get :edit, trip_id: trip.id, id: place.id
-          expect(controller.place).to eq place
-        end
+      it 'exposes place' do
+        place = create(:place, trip: trip)
+        get :show, trip_id: trip.id, id: place.id
+        expect(controller.place).to eq place
       end
     end
 
-    context 'not logged user' do
+    describe 'GET #index' do
+      it 'it renders the :index template' do
+        get :index, trip_id: trip.id
+        expect(response).to render_template :index
+      end
+    end
 
+    describe 'GET #new' do
+      it 'renders the :new template' do
+        get :new, trip_id: trip.id
+        expect(response).to render_template :new
+      end
+      it 'exposes place' do
+        place = create(:place, trip: trip)
+        get :new, trip_id: trip.id, id: place.id
+        expect(controller.place).to eq place
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'renders the :edit template' do
+        place = create(:place, trip: trip)
+        get :edit, trip_id: trip.id, id: place.id
+        expect(response).to render_template :edit
+      end
+      it 'exposes place' do
+        place = create(:place, trip: trip)
+        get :edit, trip_id: trip.id, id: place.id
+        expect(controller.place).to eq place
+      end
+    end
+  end
+
+  context 'not logged user' do
+    let(:trip) { create(:trip) }
+
+    describe '#GET show' do
+      let!(:place) { create(:place, trip: trip) }
+      it 'not render :show template' do
+        get :show, trip_id: trip.id, id: place.id
+        expect(response).not_to render_template :show
+      end
+    end
+
+    describe '#GET index' do
+      it 'not render :index template' do
+        get :index, trip_id: trip.id
+        expect(response).not_to render_template :index
+      end
+    end
+
+    describe '#GET new' do
+      it 'not render :new template' do
+        get :new, trip_id: trip.id
+        expect(response).not_to render_template :new
+      end
+    end
+
+    describe '#GET edit' do
+      let!(:place) { create(:place, trip: trip) }
+      it 'not render :edit template' do
+        get :edit, trip_id: trip.id, id: place.id
+        expect(response).not_to render_template :edit
+      end
     end
   end
 end
